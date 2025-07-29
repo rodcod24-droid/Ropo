de329f5968422ee8fbcd81edec329368d5fbf9e3
rodcod24-droid <rodcod24@gmail.com>
2025-07-28 21:37:55 -0700

    Update AnimeflvnetProviderPlugin.kt

--- AnimeflvnetProvider/src/main/kotlin/com/lagradost/AnimeflvnetProviderPlugin.kt
+++ AnimeflvnetProvider/src/main/kotlin/com/lagradost/AnimeflvnetProviderPlugin.kt
@@ -2,7 +2,7 @@
 package com.lagradost
 
 import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
-import com.lagradost.cloudstream3.plugins.BasePlugin
+import com.lagradost.cloudstream3.plugins.Plugin
 import android.content.Context
 
 @CloudstreamPlugin
